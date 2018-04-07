82e0d9421a37b57724fc15561048d2d0a465a8fa
Justin <jtleaming@hotmail.com`>
2018-04-06 20:28:43 -0600

    Adding loading functionality to buttons.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import React, { Component } from 'react';
-import { Button } from 'reactstrap'
+import { Button } from 'reactstrap';
 import axios from 'axios';
 import './App.css';
+
+class SendGifButton extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            loading: false
+        };
+    };
+    sendGif = function (number) {
+        this.setState({loading:true});
+        axios.post('http://localhost:5002/api/sendmessage/' + number + '/' + this.props.gifUri, { headers: { "Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers": " Origin, X-Requested-With, Content-Type, Accept" } })
+          .then((response) =>  {
+            console.log(response);
+            this.setState(() => ({ loading: false}));
+          })
+          .catch((error) => {
+              alert(error);
+              this.setState(({ loading: false}));
+            });
+      };
+    render(){
+        return(
+        <div>
+                <Button className="btn" onClick={() => this.sendGif(this.props.phoneNumbers)  }><i className={this.state.loading ? "heart fa fa-heart fa-4x fa-beat" : "heart fa fa-heart fa-4x"}></i>{this.props.messages}</Button>
+        </div>
+        );
+    }
+}
 
 const GifContainer = (props) => {
   const gif = 'https://giphy.com/embed/' + props.gifUri;
@@ -17,25 +45,19 @@ class App extends Component {
     super(props);
     this.state = {
       gifUri: '',
-      displayGif: false
+      displayGif: false,
+      loading: false,
+      phoneNumbers: ['18014140392', '18014034602'],
+      messages: ['Send some love to Breana...', 'Send some love to Justin...']
     };
     this.selectRandomGif = this.selectRandomGif.bind(this);
   }
-  sendGif = (number) => {
-    axios.get('http://localhost:5002/api/sendmessage/' + number + '/' + this.state.gifUri, { headers: { "Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers": " Origin, X-Requested-With, Content-Type, Accept" } })
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        alert(error);
-      });
-  };
   selectRandomGif = () => {
     axios.get('http://localhost:5000/api/sendgif', { headers: { "Access-Control-Allow-Origin": "http://localhost:3000", "Access-Control-Allow-Headers": " X-Requested-With,content-type", 'Access-Control-Allow-Credentials': true, 'Access-Control-Allow-Methods': 'GET' } })
       .then((response) => {
         this.setState(prevState => ({
           gifUri: response.data,
-          displayGif: !prevState.displayGif
+          displayGif: true
         }));
       })
       .catch(function (error) {
@@ -47,9 +69,9 @@ class App extends Component {
     return (
       <div className="">
         <h1 className="">Welcome! Send a gif!</h1>
-        <Button className="btn" onClick={() => this.sendGif('18014140392')}><i className="heart fa fa-heart fa-4x fa-beat"></i>Send some love to Breana...</Button>
-        <Button className="btn" onClick={() => this.sendGif('18014034602')}><i className="heart fa fa-heart fa-4x fa-beat"></i>Send some love Justin...</Button>
-
+        {this.state.phoneNumbers.map((phoneNumber, i)=>
+            <SendGifButton key={i} phoneNumbers={phoneNumber} gifUri={this.state.gifUri} messages={this.state.messages[i]}/>
+        )}
         <div className=""><Button className="btn1" onClick={this.selectRandomGif}>Select Random Gif</Button></div>
         {displayGif ?
           <GifContainer gifUri={this.state.gifUri} /> :
